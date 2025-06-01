609292e5abb49eda066851896b95a452c1a74879
Minhyuk Kim <rhrlwld@gmail.com>
2025-06-01 20:09:29 +0900

    chore: 초기 프로젝트 환경설정

---
+++ src/main/java/com/example/emotion_storage/EmotionStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.emotion_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/emotion_storage/EmotionStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emotion_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b8b164a16404c617479274f9567ac81bb8ad7c2d
JoonKyoLee <joonkyolee@gmail.com>
2025-07-12 23:20:49 +0900

    feat: 전역 예외 처리를 위한 ErrorCode enum 정의

---
+++ src/main/java/com/example/emotion_storage/global/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.example.emotion_storage.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    // 예시
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}

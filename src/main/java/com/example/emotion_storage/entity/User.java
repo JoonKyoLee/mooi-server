097732ebdbc94836cc0562202a4acbfab2fc06b1
JoonKyoLee <joonkyolee@gmail.com>
2025-07-24 11:46:05 +0900

    feat: 소셜 로그인 기반 User 엔티티 추가

---
+++ src/main/java/com/example/emotion_storage/entity/User.java
@@ -0,0 +1,38 @@
+package com.example.emotion_storage.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SocialType socialType;
+
+    @Column(nullable = false)
+    private String socialId;
+
+    private String email;
+
+    private String profileImage;
+}

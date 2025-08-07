cba7f39c7dca2cf093c52b92e3a39cd154f08c0d
JoonKyoLee <joonkyolee@gmail.com>
2025-08-07 23:46:19 +0900

    feat: 소셜 로그인 관련 에러 메시지 추가

--- src/main/java/com/example/emotion_storage/global/exception/ErrorCode.java
+++ src/main/java/com/example/emotion_storage/global/exception/ErrorCode.java
@@ -7,10 +7,13 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    // 예시
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
 
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    ALREADY_REGISTERED_WITH_KAKAO(HttpStatus.BAD_REQUEST, "해당 이메일은 카카오로 가입된 계정입니다."),
+
+    INVALID_ID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 ID TOKEN 입니다.")
+    ;
 
     private final HttpStatus httpStatus;
     private final String message;

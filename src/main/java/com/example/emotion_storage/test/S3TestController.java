7da7e28b9e91f6296499f182be7bd6241e3a5ffe
Minhyuk Kim <rhrlwld@gmail.com>
2025-08-08 18:37:07 +0900

    feat: Swagger UI 적용

--- src/main/java/com/example/emotion_storage/test/S3TestController.java
+++ src/main/java/com/example/emotion_storage/test/S3TestController.java
@@ -1,5 +1,7 @@
 package com.example.emotion_storage.test;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "AWS S3 테스트", description = "이미지 업로드 테스트 API")
 @Slf4j
 @RestController
 @RequestMapping("/s3")
@@ -16,6 +19,7 @@ public class S3TestController {
 
     private final S3TestService testS3Service;
 
+    @Operation(summary = "이미지 업로드", description = "이미지를 업로드합니다.")
     @PostMapping("/upload")
     public String uploadImage(@RequestParam("file") MultipartFile file) {
         try {
--- src/main/java/com/example/emotion_storage/test/TestController.java
+++ src/main/java/com/example/emotion_storage/test/TestController.java
@@ -1,11 +1,15 @@
 package com.example.emotion_storage.test;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "테스트", description = "테스트 API")
 @RestController
 public class TestController {
 
+    @Operation(summary = "테스트 API", description = "Hello, mooi String을 반환합니다.")
     @GetMapping("/hello")
     public String hello() {
         return "Hello, mooi!";

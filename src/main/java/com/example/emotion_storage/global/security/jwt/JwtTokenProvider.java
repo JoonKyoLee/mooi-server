18585ce1eac654fa85737dff52e4a4c65ffafb8c
JoonKyoLee <joonkyolee@gmail.com>
2025-08-09 04:52:04 +0900

    feat: JWT 토큰 생성, 검증, 파싱 로직 추가

---
+++ src/main/java/com/example/emotion_storage/global/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,67 @@
+package com.example.emotion_storage.global.security.jwt;
+
+import com.example.emotion_storage.global.exception.principal.TokenStatus;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+
+    private final Key secretKey;
+
+    private final int jwtExpiration;
+
+    public JwtTokenProvider(
+            @Value("${jwt.secret}") String jwtSecret,
+            @Value("${jwt.access-token.expiration-minutes}") int jwtExpiration
+    ) {
+        this.secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+        this.jwtExpiration = jwtExpiration;
+    }
+
+    public String generateAccessToken(Long userId) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + TimeUnit.MINUTES.toMillis(jwtExpiration));
+
+        return Jwts.builder()
+                .setSubject(userId.toString())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public TokenStatus validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return TokenStatus.VALID;
+        } catch (ExpiredJwtException e) {
+            return TokenStatus.EXPIRED;
+        } catch (JwtException e) {
+            return TokenStatus.INVALID;
+        }
+    }
+
+    public Long getUserIdFromToken(String token) {
+        Claims claims =  Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return Long.parseLong(claims.getSubject());
+    }
+}

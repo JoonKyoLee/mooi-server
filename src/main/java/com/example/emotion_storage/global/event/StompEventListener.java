280a248442533361cf40e202b771063a4dddba7f
JoonKyoLee <joonkyolee@gmail.com>
2025-09-08 17:25:40 +0900

    chore: STOMP 연결/해제 이벤트 리스너 추가 - SessionConnectedEvent, SessionDisconnectEvent 발생 시 로그 기록 - 세션 단위 연결 및 종료 상태 추적 가능

---
+++ src/main/java/com/example/emotion_storage/global/event/StompEventListener.java
@@ -0,0 +1,29 @@
+package com.example.emotion_storage.global.event;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Slf4j
+@Component
+public class StompEventListener {
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+
+        log.info("세션 {}이 STOMP 웹소켓에 연결되었습니다.", sessionId);
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+
+        log.info("세션 {}이 STOMP 웹소켓 연결을 종료했습니다.", sessionId);
+    }
+}

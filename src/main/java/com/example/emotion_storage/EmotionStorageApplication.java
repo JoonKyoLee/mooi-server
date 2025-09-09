8a3d8e92100c606c95316780f5c348d9c1df6ea7
JoonKyoLee <joonkyolee@gmail.com>
2025-09-09 21:59:27 +0900

    hotfix: @EnableJpaAuditing 추가

--- src/main/java/com/example/emotion_storage/EmotionStorageApplication.java
+++ src/main/java/com/example/emotion_storage/EmotionStorageApplication.java
@@ -2,7 +2,9 @@ package com.example.emotion_storage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class EmotionStorageApplication {
 

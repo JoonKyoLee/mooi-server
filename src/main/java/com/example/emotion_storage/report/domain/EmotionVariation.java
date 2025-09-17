de3336d09c83ca6d49435a77fc62c3944f6340bc
Minhyuk Kim <rhrlwld@gmail.com>
2025-09-18 01:26:09 +0900

    chore: emotion_variation 테이블 수정에 따른 엔티티 수정

--- src/main/java/com/example/emotion_storage/report/domain/EmotionVariation.java
+++ src/main/java/com/example/emotion_storage/report/domain/EmotionVariation.java
@@ -4,6 +4,8 @@ import com.example.emotion_storage.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Table(name = "emotion_variation")
@@ -14,15 +16,18 @@ public class EmotionVariation extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "expectation_id")
+    @Column(name = "emotion_variation_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "report_id", nullable = false)
     private Report report;
 
-    @Column(nullable = false, length = 500)
-    private String expectation;
+    @Column(nullable = false)
+    private LocalDateTime time;
+
+    @Column(nullable = false, length = 30)
+    private String label;
 
     public void setReport(Report report) {
         this.report = report;

5643d3fd6a85dea5296ea0caf8bae1ee0d7faac9
JoonKyoLee <joonkyolee@gmail.com>
2025-09-19 16:32:56 +0900

    feat: 타임캡슐 목록 조회 시에 기록 날짜, 상태도 반환하도록 추가

--- src/main/java/com/example/emotion_storage/timecapsule/dto/TimeCapsuleDto.java
+++ src/main/java/com/example/emotion_storage/timecapsule/dto/TimeCapsuleDto.java
@@ -2,21 +2,27 @@ package com.example.emotion_storage.timecapsule.dto;
 
 import com.example.emotion_storage.timecapsule.domain.AnalyzedEmotion;
 import com.example.emotion_storage.timecapsule.domain.TimeCapsule;
+import com.example.emotion_storage.timecapsule.domain.TimeCapsuleStatus;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record TimeCapsuleDto(
         Long id,
+        LocalDateTime historyDate,
         LocalDateTime createdAt,
         LocalDateTime updatedAt,
         LocalDateTime openAt,
         Boolean isFavorite,
         List<String> emotions,
-        String title
+        String title,
+        String timeCapsuleStatus
 ) {
     public static TimeCapsuleDto of(TimeCapsule entity) {
+        TimeCapsuleStatus status = TimeCapsuleStatus.getStatus(entity);
+
         return new TimeCapsuleDto(
                 entity.getId(),
+                entity.getHistoryDate(),
                 entity.getCreatedAt(),
                 entity.getUpdatedAt(),
                 entity.getOpenedAt(),
@@ -25,7 +31,8 @@ public record TimeCapsuleDto(
                         .stream()
                         .map(AnalyzedEmotion::getAnalyzedEmotion)
                         .toList(),
-                entity.getOneLineSummary()
+                entity.getOneLineSummary(),
+                status.getStatusMessage()
         );
     }
 }

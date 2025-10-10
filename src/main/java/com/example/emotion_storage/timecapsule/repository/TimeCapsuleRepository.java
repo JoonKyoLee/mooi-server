67e2df74af97de32f7d04a0a7514ed4aa2d193f7
JoonKyoLee <joonkyolee@gmail.com>
2025-10-11 03:17:22 +0900

    feat: 채팅방 아이디와 임시저장 상태를 바탕으로 타임캡슐을 조회하는 로직 추가

--- src/main/java/com/example/emotion_storage/timecapsule/repository/TimeCapsuleRepository.java
+++ src/main/java/com/example/emotion_storage/timecapsule/repository/TimeCapsuleRepository.java
@@ -3,6 +3,7 @@ package com.example.emotion_storage.timecapsule.repository;
 import com.example.emotion_storage.timecapsule.domain.TimeCapsule;
 import java.sql.Date;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,6 +15,8 @@ import java.util.List;
 
 @Repository
 public interface TimeCapsuleRepository extends JpaRepository<TimeCapsule, Long> {
+
+    Optional<TimeCapsule> findByChatroomIdAndIsTempSaveTrue(Long chatroomId);
     
     @Query("SELECT tc FROM TimeCapsule tc WHERE tc.user.id = :userId AND tc.isOpened = false AND tc.deletedAt IS NULL")
     List<TimeCapsule> findUnopenedTimeCapsulesByUserId(@Param("userId") Long userId);

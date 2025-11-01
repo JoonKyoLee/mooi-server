2f9e7d9ccf05d58ed19ab1f8a4113fa3f9600132
JoonKyoLee <joonkyolee@gmail.com>
2025-11-01 22:22:56 +0900

    feat: 현재 채팅방의 이전 채팅방을 조회하는 메서드 추가

--- src/main/java/com/example/emotion_storage/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/example/emotion_storage/chat/repository/ChatRoomRepository.java
@@ -1,12 +1,22 @@
 package com.example.emotion_storage.chat.repository;
 
 import com.example.emotion_storage.chat.domain.ChatRoom;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
     Optional<ChatRoom> findTopByUser_IdOrderByCreatedAtDesc(Long userId);
+
+    @Query(
+            "SELECT cr from ChatRoom cr " +
+            "WHERE cr.user.id = :userId " +
+            "AND (cr.createdAt < :createdAt or (cr.createdAt = :createdAt AND cr.id < :currentChatRoomId)) " +
+            "ORDER BY cr.createdAt DESC, cr.id DESC"
+    )
+    Optional<ChatRoom> findPrevRoom(Long userId, LocalDateTime createdAt, Long currentChatRoomId);
 }

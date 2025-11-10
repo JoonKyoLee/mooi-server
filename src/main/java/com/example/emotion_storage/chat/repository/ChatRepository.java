22338b8411b0819379ba3eb54878c6c8d22f0e28
JoonKyoLee <joonkyolee@gmail.com>
2025-11-11 01:55:45 +0900

    feat: 채팅방 내 모든 메시지 조회용 Repository 메서드 추가

--- src/main/java/com/example/emotion_storage/chat/repository/ChatRepository.java
+++ src/main/java/com/example/emotion_storage/chat/repository/ChatRepository.java
@@ -1,9 +1,19 @@
 package com.example.emotion_storage.chat.repository;
 
 import com.example.emotion_storage.chat.domain.Chat;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
+
+    @Query(
+            "SELECT c FROM Chat c " +
+            "WHERE c.chatRoom.id = :roomId " +
+            "ORDER BY c.chatTime ASC, c.id ASC "
+    )
+    List<Chat> findAllByRoomIdOrderByTimeAsc(@Param("roomId") Long roomId);
 }

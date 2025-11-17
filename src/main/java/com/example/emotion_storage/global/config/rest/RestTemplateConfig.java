02fb4377f7f4279fc2da0a19d12b37e65e1032e4
Minhyuk Kim <rhrlwld@gmail.com>
2025-11-17 23:36:09 +0900

    fix: Read timeout 증가를 통한 AI 서버 통신 시간 확보

--- src/main/java/com/example/emotion_storage/global/config/rest/RestTemplateConfig.java
+++ src/main/java/com/example/emotion_storage/global/config/rest/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.example.emotion_storage.global.config.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -8,11 +9,17 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
 
+    @Value("${rest-template.connect-timeout:5000}")
+    private int connectTimeout;
+
+    @Value("${rest-template.read-timeout:120000}")
+    private int readTimeout;
+
     @Bean
     public RestTemplate restTemplate() {
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectTimeout(5000);
-        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(connectTimeout);
+        factory.setReadTimeout(readTimeout);
         return new RestTemplate(factory);
     }
 }
